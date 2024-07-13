4556c6ac0cac6ec712381756e48619de7636ceb5
sillycowvalley <89331909+sillycowvalley@users.noreply.github.com>
2024-07-13 20:43:01 +1000

    Update VGMDemo.hs

--- Source/Projects/VGM/VGMDemo.hs
+++ Source/Projects/VGM/VGMDemo.hs
@@ -3,10 +3,13 @@ program SNVGM
     uses "/Source/Library/Boards/Hopper6502"
     uses "/Source/Library/Audio/SNVGMPlayer"
 
-    //uses "Samples/Ghost"
+    uses "Samples/Ghost"
+    //uses "Samples/GB"
     //uses "Samples/Christmas"
     //uses "Samples/Sonata"
-    uses "Samples/AlleyCat"
+    //uses "Samples/AlleyCat"
+    //uses "Samples/Kings"
+    //uses "Samples/BruceLee"
         
     Hopper()
     {
