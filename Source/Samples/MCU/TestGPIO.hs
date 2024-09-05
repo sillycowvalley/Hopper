7aca53f68b50a299c792ed472dd071aa414ad3e8
sillycowvalley <89331909+sillycowvalley@users.noreply.github.com>
2024-09-05 10:22:37 +1000

    Update TestGPIO.hs

--- Source/Samples/MCU/TestGPIO.hs
+++ Source/Samples/MCU/TestGPIO.hs
@@ -1,25 +1,26 @@
 program TestGPIO
 {
     //uses "/Source/Library/Boards/AdafruitFeather"
-    uses "/Source/Library/Boards/AdafruitItsyBitsy"
+    //uses "/Source/Library/Boards/AdafruitItsyBitsy"
     //uses "/Source/Library/Boards/AdafruitQTPy"
     //uses "/Source/Library/Boards/Challenger2040WiFi"
+    
+    uses "/Source/Library/Boards/SparkfunProMicroRP2040"
         
     Hopper()
     {
         
-        byte pin = GP11;
+        byte pin = GP21;
         PinMode(pin, PinModeOption.Output);
         
+        /*        
         byte button = GP13;
         PinMode(pin, PinModeOption.InputPulldown);
-        
-        
         loop
         {
             DigitalWrite(pin, DigitalRead(button));
         }
-        
+        */
         
         for (byte iterations = 0; iterations < 10; iterations++)
         {
