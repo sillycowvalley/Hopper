8c2d40635d8caf1275118559d840dcbe60b28a33
sillycowvalley <89331909+sillycowvalley@users.noreply.github.com>
2024-09-16 16:26:38 +1000

    Update Blink.hs

--- Source/Samples/MCU/Blink.hs
+++ Source/Samples/MCU/Blink.hs
@@ -3,19 +3,17 @@ program Blink
     //uses "/Source/Library/Boards/PiPico"
     //uses "/Source/Library/Boards/Challenger2040WiFi"
     //uses "/Source/Library/Boards/Challenger2040SDRTC"
-    uses "/Source/Library/Boards/ChallengerNB2040WiFi"
+    //uses "/Source/Library/Boards/ChallengerNB2040WiFi"
     //uses "/Source/Library/Boards/Challenger2350WiFi6Ble5"
     //uses "/Source/Library/Boards/PimoroniTiny2350"
-    //uses "/Source/Library/Boards/Hopper6502"
+    uses "/Source/Library/Boards/Hopper6502"
     
     {
         loop
         {
             Delay(500);
             
-            InterruptsEnabled = false;
             LED = !LED;
-            InterruptsEnabled = true;
             
             WriteLn((Time.Seconds).ToString() + " seconds");
         }
