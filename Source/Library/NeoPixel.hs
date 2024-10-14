295fcc25880d3497604f21abf05ed46b4001b906
sillycowvalley <89331909+sillycowvalley@users.noreply.github.com>
2024-10-14 19:31:16 +1100

    NeoPixel.SetHue

--- Source/Library/NeoPixel.hs
+++ Source/Library/NeoPixel.hs
@@ -127,61 +127,43 @@ unit NeoPixel
         Clear();
     }
 #endif  
-    HueToRGB(long hue, ref byte r, ref byte g, ref byte b)
+
+
+                           
+    SetHue(uint pixel, byte hue)
     {
-        // Normalize hue to 0-0xFFFF
-        hue = hue % 0x10000;
+        byte sector = hue / 43;  // Divide the hue into 6 sectors (256 / 6 ~ 43)
+        uint remainder = (hue % 43) * 6;
     
-        // Each 1/6th of the range represents a primary color sector
-        uint sector    = uint(hue / 0x2AAA); // 0x10000 / 6 = 0x2AAA
-        uint remainder = uint(hue % 0x2AAA); // Hue within the sector
+        byte q = byte(((255 * (255 - remainder)) / 255) & 0xFF);
+        byte t = byte(((255 * remainder) / 255) & 0xFF);
     
-        long q = (long(255) * (0x2AAA - remainder)) / 0x2AAA;
-        long t = (long(255) * remainder) / 0x2AAA;
-    
-        switch(sector)
+        // Properly using statement blocks in switch cases
+        switch (sector)
         {
             case 0:
             {
-                // Red to yellow
-                r = 255;
-                g = t.GetByte(0);
-                b = 0;
+                NeoPixel.SetColor(pixel, 255, t, 0);
             }
             case 1:
             {
-                // Yellow to green
-                r = q.GetByte(0);
-                g = 255;
-                b = 0;
+                NeoPixel.SetColor(pixel, q, 255, 0);
             }
             case 2:
             {
-                // Green to cyan
-                r = 0;
-                g = 255;
-                b = t.GetByte(0);
+                NeoPixel.SetColor(pixel, 0, 255, t);
             }
             case 3:
             {
-                // Cyan to blue
-                r = 0;
-                g = q.GetByte(0);
-                b = 255;
+                NeoPixel.SetColor(pixel, 0, q, 255);
             }
             case 4:
             {
-                // Blue to magenta
-                r = t.GetByte(0);
-                g = 0;
-                b = 255;
+                NeoPixel.SetColor(pixel, t, 0, 255);
             }
             case 5:
             {
-                // Magenta to red
-                r = 255;
-                g = 0;
-                b = q.GetByte(0);
+                NeoPixel.SetColor(pixel, 255, 0, q);
             }
         }
     }
--- Source/Samples/MCU/RainbowNeo.hs
+++ Source/Samples/MCU/RainbowNeo.hs
@@ -17,25 +17,20 @@ program RainbowNeo
         
         uint totalPixels = NeoPixel.Length;
         
-        long hueLimit = 0x10000 * totalPixels;
-        
         byte r;
         byte g;
         byte b;
         
         loop
         {   
-            for (long firstPixelHue = 0; firstPixelHue < hueLimit; firstPixelHue += 256) 
+            for (uint firstPixelHue = 0; firstPixelHue < 256; firstPixelHue ++) 
             {
                 for (byte pixel = 0; pixel < totalPixels; pixel++)
                 {
-                    
-                    long pixelHue = firstPixelHue + (pixel * 0x10000 / totalPixels);
-                    NeoPixel.HueToRGB(pixelHue, ref r, ref g, ref b);
-                    NeoPixel.SetColor(pixel, r, g, b);
+                    uint pixelHue = firstPixelHue + (pixel * 256 / totalPixels);
+                    NeoPixel.SetHue(pixel, byte(pixelHue % 256));
                 }
                 NeoPixel.Show();
-                Time.Delay(100);
             }
         }
     }
