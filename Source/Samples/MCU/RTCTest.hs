91dbbc33a0b036cb11273b5a58072b9e603d739c
Michael Cartwright <michael@sillycowvalley.com>
2024-12-04 22:49:41 +1300

    Update RTCTest.hs

--- Source/Samples/MCU/RTCTest.hs
+++ Source/Samples/MCU/RTCTest.hs
@@ -14,33 +14,27 @@ program RTCFeather
         }
         
         // this will only update the time if we are running in debugger
-        _ = RTC.SetFromDebugger();
+        _ = RTC.SetFromDebugger(); 
         
         string lastUpdate;
         loop
         {
             string time = RTC.Time;
-            if (lastUpdate != time)
+            if (lastUpdate != time) // seconds changed ..
             {
                 lastUpdate = time;
-                
-                string date = RTC.Date;
+                string diagnosticOutput = RTC.Date + ", " + time;
                 
 #ifdef RTC_HAS_TEMPERATURE                
                 // this RTC board also has a temperature sensor
-                float temperature = RTC.Temperature;
-                string temp = temperature.ToString() + "C"; 
-                
-                // diagnostics to serial console
-                IO.WriteLn(date + " " + time + " " + temp); 
-#else
-                IO.WriteLn(date + " " + time); 
-#endif                
+                diagnosticOutput += ", " + (RTC.Temperature).ToString() + "C"; 
+#endif
+                IO.WriteLn(diagnosticOutput); 
             }
-            Time.Delay(250);
             
             // heartbeat using built-in LED
-            LED = !LED; 
+            Time.Delay(250);
+            LED = !LED;
         }
     }
 }
