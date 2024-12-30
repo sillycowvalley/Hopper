717ffcaee3957205791cfec299219e784f76e580
Michael Cartwright <michael@sillycowvalley.com>
2024-12-31 12:40:22 +1300

    Update Blink.hs

--- Source/Samples/MCU/Blink.hs
+++ Source/Samples/MCU/Blink.hs
@@ -2,15 +2,22 @@ program Blink
 {
     //uses "/Source/Library/Boards/PiPico2"
     //uses "/Source/Library/Boards/PimoroniPicoPlus2W"
-    uses "/Source/Library/Boards/AdafruitFeatherRP2350Hstx"
+    //uses "/Source/Library/Boards/AdafruitFeatherRP2350Hstx"
+    uses "/Source/Library/Boards/PimoroniTiny2350"
     
     Hopper()
     {
+        //bool on;
+        //MCU.PinMode(GP5, MCU.PinModeOption.Output);
+        
         loop
         {
             Delay(500);
             
             LED = !LED;
+            
+            //DigitalWrite(GP5, on);
+            //on = !on;
             
             WriteLn((Time.Seconds).ToString() + " seconds");
         }
