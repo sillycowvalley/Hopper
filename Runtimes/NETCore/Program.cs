3b5317b84213693d2785504c840ad4625cacb86e
sillycowvalley <89331909+sillycowvalley@users.noreply.github.com>
2025-07-02 09:12:17 +1000

    Hopper path resolution

--- Runtimes/NETCore/Program.cs
+++ Runtimes/NETCore/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using HopperRuntime.Core;
 
@@ -9,10 +12,31 @@ namespace HopperRuntime
     public class Program
     {
         public static DateTime LaunchTime { get; private set; }
+        public static List<String> Arguments { get; private set; } = new List<String>();
         public static bool TraceEnabled { get; private set; } = false;
+        public static string HopperRootFolder { get; private set; } = String.Empty;
 
         public static void Main(string[] args)
         {
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+            string exePath = Environment.ProcessPath ?? String.Empty;
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(exePath);
+            string versionName = fileVersionInfo.FileVersion ?? String.Empty;
+            Console.Title = exePath + " [" + versionName + "]";
+
+            string rootFolder = exePath ?? String.Empty;
+            while (!rootFolder.EndsWith("Hopper"))
+            {
+                rootFolder = Path.GetDirectoryName(rootFolder) ?? String.Empty;
+                if (String.IsNullOrEmpty(rootFolder)) { break; }
+            }
+            if (!String.IsNullOrEmpty(rootFolder))
+            {
+                Directory.SetCurrentDirectory(rootFolder);
+                HopperRootFolder = rootFolder;
+            }
+
             LaunchTime = DateTime.Now;
 
             PowerShellAnsiEnabler.EnableAnsiSupport();
@@ -31,13 +55,40 @@ namespace HopperRuntime
                 {
                     TraceEnabled = true;
                 }
-                else
+                else if (programFile == null)
                 {
                     programFile = arg;
                 }
+                else
+                {
+                    Arguments.Add(arg);
+                }
             }
 
-            if (programFile == null)
+            if (String.IsNullOrEmpty(programFile))
+            {
+                Console.WriteLine("Usage: Hopper <program.hexe>");
+                return;
+            }
+            string extension = Path.GetExtension(programFile).ToLower();
+            if (String.IsNullOrEmpty(extension))
+            {
+                programFile += ".hexe";
+            }
+            string launchFolder = Path.GetDirectoryName(programFile) ?? String.Empty;
+            if (String.IsNullOrEmpty(launchFolder))
+            {
+                string exeFolder = Path.GetDirectoryName(exePath) ?? String.Empty;
+                if (!String.IsNullOrEmpty(exeFolder) && File.Exists(Path.Combine(exeFolder, programFile)))
+                {
+                    programFile = Path.Combine(exeFolder, programFile);
+                }
+                else
+                {
+                    programFile = Path.Combine("Bin", programFile);
+                }
+            }
+            if (!File.Exists(programFile))
             {
                 Console.WriteLine("Usage: Hopper <program.hexe>");
                 return;
