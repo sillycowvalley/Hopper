6d563485cb04e1cc5b243cca78fbb440b5801f58
sillycowvalley <89331909+sillycowvalley@users.noreply.github.com>
2025-07-07 12:00:59 +1000

    Update Blink.hs

--- Source/Samples/MCU/Blink.hs
+++ Source/Samples/MCU/Blink.hs
@@ -1,26 +1,17 @@
 program Blink
 {
-    //uses "/Source/Library/Boards/PiPico2"
+    uses "/Source/Library/Boards/Challenger2350WiFi6Ble5"
     //uses "/Source/Library/Boards/PimoroniPicoPlus2W"
     //uses "/Source/Library/Boards/AdafruitFeatherRP2350Hstx"
-    uses "/Source/Library/Boards/PimoroniTiny2350"
+    //uses "/Source/Library/Boards/PimoroniTiny2350"
     
     Hopper()
     {
-        bool on;
-        MCU.PinMode(GP6, MCU.PinModeOption.Output);
-        
         loop
         {
             Delay(500);
-            
             LED = !LED;
-            
-            DigitalWrite(GP6, on);
-            on = !on;
-            
             WriteLn((Time.Seconds).ToString() + " seconds");
         }
     }
 }
-
